9a0fbc6b7598ce6bdc6b1cf5bc9e72854153e0f7
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2024-11-05 17:25:49 +0200

    Update GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,1 +1,14 @@
+using UnityEngine.UI;
+using UnityEngine;
 
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int Chances;
+
+    private void Awake()
+    {
+      instance = this;
+    }
+}
