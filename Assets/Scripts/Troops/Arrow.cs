c05d2854ba78e48d308a806551427566d1578bf5
Luyando Shilukukwa <48803829+Luyandosnt@users.noreply.github.com>
2024-11-05 17:18:39 +0200

    Update Arrow.cs

--- Assets/Scripts/Troops/Arrow.cs
+++ Assets/Scripts/Troops/Arrow.cs
@@ -28,7 +28,7 @@ public class Arrow : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
-            other.GetComponent<Enemy>().TakeDamage(damage);
+            other.GetComponent<Enemy>().TakeDamage(damage, false);
             Destroy(gameObject); // Destroy the arrow on impact
         }
     }
